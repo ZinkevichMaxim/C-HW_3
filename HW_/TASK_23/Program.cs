d18aaea1d132046b7de458a0fac72652bee4ce1a
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-29 19:27:03 +0300

    task_23_program

---
+++ HW_/TASK_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите целое число");
+int nomber = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= nomber)
+{
+    int cubnomber = count * count * count;
+    Console.Write(cubnomber + " / ");
+    count = count + 1;
+}
+
+
+
+
+
